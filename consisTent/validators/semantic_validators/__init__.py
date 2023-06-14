b3fdeb851a785ca7eface5477910f30efb344ec6
dror ivry <drorivry@gmail.com>
2023-06-14 16:00:14 +0300

    matching tests

--- consisTent/validators/semantic_validators/__init__.py
+++ consisTent/validators/semantic_validators/__init__.py
@@ -1,5 +1,7 @@
 from . import consistency_validator
 from . import labels_validator
+from . import request_response_matching_validator
 
 from .labels_validator import LabelsValidator
 from .consistency_validator import ConsistencyValidator
+from request_response_matching_validator import RequestResponseValidator
